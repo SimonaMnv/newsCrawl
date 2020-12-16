b4f0011f91de63b746d6e21d305cc3bcc418b1b6
MihalisP <mihalispapak@gmail.com>
2020-12-16 21:29:12 +0200

    minor change in crawler domain & follow

--- crawling/crawling/spiders/newsbomb_spider.py
+++ crawling/crawling/spiders/newsbomb_spider.py
@@ -5,17 +5,17 @@ from scrapy.spiders import CrawlSpider
 
 class NewsbombSpider(scrapy.Spider):
     name = 'newsbomb'
-    allowed_domains = ['https://www.newsbomb.gr']
+    allowed_domains = ['newsbomb.gr']
     start_urls = ['https://www.newsbomb.gr/ellada/astynomiko-reportaz']
 
     def parse(self, response):
-        article_links = response.css('.item-title a ::attr(href)')
-        next_page = int(response.css("span.nav-number::text").get()) + 1
+        article_links = response.css('a.overlay-link ::attr(href)')
+        next_page = "https://newsbomb.gr/ellada/astynomiko-reportaz?page=" + str(
+            int(response.css("span.nav-number::text").get()) + 1)
 
         for link in article_links:
-            url = link.get()
-            if url:
-                yield response.follow(url=url, callback=self.parse_article)
+            url = 'https://www.newsbomb.gr' + link.get()
+            yield scrapy.Request(url=url, callback=self.parse_article)
         if next_page:
             yield scrapy.Request(response.urljoin(next_page), callback=self.parse)
 
