d27c20fd1e5eb2c488de48827cabe1c2c48875c8
unknown <simona.mnv@gmail.com>
2020-12-16 20:58:44 +0200

    minor // ignore this

--- api/models/article_model.py
+++ api/models/article_model.py
@@ -5,6 +5,7 @@ class ArticleOfInterest(models.Model):
     title = models.CharField(max_length=255, unique=False)
     body = models.TextField(null=False, blank=False)
     date = models.DateTimeField(null=False, blank=False)
+    tags = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return self.title
