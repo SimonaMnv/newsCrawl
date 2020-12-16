b8dda5eeab265db14db0ba9d69226d0bd2a1fff6
unknown <simona.mnv@gmail.com>
2020-12-16 15:43:16 +0200

    minor updt

--- api/models/article_model.py
+++ api/models/article_model.py
@@ -3,7 +3,8 @@ from django.db import models
 
 class ArticleOfInterest(models.Model):
     title = models.CharField(max_length=255, unique=False)
-    body = models.TextField(null=True, blank=True)
+    body = models.TextField(null=False, blank=False)
+    date = models.DateTimeField(null=False, blank=False)
 
     def __str__(self):
         return self.title
